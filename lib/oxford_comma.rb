0682e3b767b9405c58c9c4e5e06d24f992190fdc
Viviana Lugo <vivi28lu@gmail.com>
2020-09-22 02:46:43 +0000

    Automatically backed up by Learn

---
+++ lib/oxford_comma.rb
@@ -0,0 +1,10 @@
+def oxford_comma(array)
+  if array.length == 2
+        array.join(" and ")
+  elsif array.length > 2
+        array.insert(-2, "and")
+        array[0..-1].join(", ")
+  else
+      array.join
+  end
+end
