942a92792d6c09e9de3bee98ae0a5d416c04331a
Viviana Lugo <vivi28lu@gmail.com>
2020-09-22 02:48:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 2
         array.join(" and ")
   elsif array.length > 2
-        array.insert(-1, "and")
+        array.insert(-2, "and")
         array[0..1].join(", ")
   else
       array.join
