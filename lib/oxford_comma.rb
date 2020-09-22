8ec3d4ecbbfbd204219d05b9379b836c915d44e9
Viviana Lugo <vivi28lu@gmail.com>
2020-09-22 02:57:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
         array.join(" and ")
     elsif array.length > 2
         array.insert(-2, "and")
-        array[0..1].join(", ")
+        array[0..-1].join(", ")
     elsif array.length == 3
         array.insert(-2, " and ")
         array.join
